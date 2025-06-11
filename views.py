34a2677b2072267023fefb4c9d693da2d61b3929
MatheusW14 <matheuscostamendes@hotmail.com>
2025-06-10 23:51:40 -0300

    adiciona funcionalidade de upload de imagens e refatora templates e configuração

--- views.py
+++ views.py
@@ -1,4 +1,12 @@
-from flask import render_template, request, redirect, session, flash, url_for
+from flask import (
+    render_template,
+    request,
+    redirect,
+    session,
+    flash,
+    url_for,
+    send_from_directory,
+)
 from jogoteca import app, db
 from models import Jogos, Usuarios
 
@@ -36,6 +44,10 @@ def criar():
     novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
     db.session.add(novo_jogo)
     db.session.commit()
+
+    arquivo = request.files["arquivo"]
+    upload_path = app.config["UPLOAD_PATH"]
+    arquivo.save(f"{upload_path}/capa{novo_jogo.id}.jpg")
 
     return redirect(url_for("index"))
 
@@ -107,3 +119,8 @@ def logout():
     session["usuario_logado"] = None
     flash("Logout efetuado com sucesso!")
     return redirect(url_for("index"))
+
+
+@app.route("/uploads/<nome_arquivo>")
+def imagem(nome_arquivo):
+    return send_from_directory("uploads", nome_arquivo)
